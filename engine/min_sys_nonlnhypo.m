64b12ba8062572c24255683fbc0dfc73c63ad2dd
Tianshu Bao <tianshubao11@gmail.com>
2021-03-18 15:06:17 -0500

    Add files via upload

--- engine/min_sys_nonlnhypo.m
+++ engine/min_sys_nonlnhypo.m
@@ -4,11 +4,15 @@ function [minlist, fmin] = min_sys_nonlnhypo(u_min, u_max, deltat, deltax, xlist
     dt = deltat;
     mesh = size(xlist);
     m = mesh(2);     
-    minlist_1 = zeros(3,m);     %for rho
-    minlist_2 = zeros(3,m);     %for momemtum
-    minlist_3 = zeros(3,m);     %for energy
+    minlist_1 = zeros(1,m);     %for rho
+    minlist_2 = zeros(1,m);     %for momemtum
+    minlist_3 = zeros(1,m);     %for energy
+    minlist = [minlist_1; minlist_2; minlist_3];
+    minlist(1, 1) = 0.5; %u_min(1, 1);
+    minlist(1, m) = 0.3; %u_min(1, m);
     
-    function result = fun(rho, mmt, engy, gamma) %flux function
+    
+    function result = fun(rho, mmt, engy, gamma) %flux function, is a 3 * 1 vector
         f1 = mmt;
         f2 = 1/2*(3 - gamma)*mmt^2/rho + (gamma - 1)*engy;
         f3 = gamma*mmt*engy/rho - 1/2*(gamma - 1)*mmt^2/(rho^2);
@@ -17,7 +21,7 @@ function [minlist, fmin] = min_sys_nonlnhypo(u_min, u_max, deltat, deltax, xlist
 
     tgt = 0;
     
-    for i = 1 : 3       % 3 variables for euler equations
+     % 3 variables for euler equations
         
         for j = 2 : m - 1  %construct min function
 
@@ -43,93 +47,118 @@ function [minlist, fmin] = min_sys_nonlnhypo(u_min, u_max, deltat, deltax, xlist
             e2 = str2sym(engy_2);        
             e3 = str2sym(engy_3);
             
-            k1 = [r1; m1; e1];
+            k1 = [r1; m1; e1];      %combine them to a 3*1 vector
             k2 = [r2; m2; e2];
             k3 = [r3; m3; e3];
+            u = [r1, r2, r3, m1, m2, m3, e1, e2, e3]; 
             
-            if j == 2
-                u = [k1, k2, k3];
-            else
-                u = [u, k3];
-            end
+%             if j == 2
+%                 u = [k1, k2, k3];
+%             else
+%                 u = [u, k3];
+%             end
 
-            u_1 = (k3 + k2)/2 - dt/(2 * dx) * (fun(k3(1), k3(2), k3(3), gamma) - fun(k2(1), k2(2), k2(3), gamma));  %u_+1/2
+            u_1 = (k3 + k2)/2 - dt/(2 * dx) * (fun(k3(1), k3(2), k3(3), gamma) - fun(k2(1), k2(2), k2(3), gamma));  %u_+1/2     3*1 vector
             u_2 = (k2 + k1)/2 - dt/(2 * dx) * (fun(k2(1), k2(2), k2(3), gamma) - fun(k1(1), k1(2), k1(3), gamma));  %u_-1/2
 
-            tgt_new = k2 - dt/dx * (fun(u_1(1), u_1(2), u_1(3), gamma) - fun(u_2(1), u_2(2), u_2(3), gamma));     %f(u_i+1/2)-f(u_i-1/2)    
+            tgt = k2 - dt/dx * (fun(u_1(1), u_1(2), u_1(3), gamma) - fun(u_2(1), u_2(2), u_2(3), gamma));     %f(u_i+1/2)-f(u_i-1/2)    3*1 vector
 
-            tgt = tgt_new + tgt;
+%             tgt = tgt_new + tgt;
+            
+            ht_1 = matlabFunction(tgt(1),'vars',{u});
+            ht_2 = matlabFunction(tgt(2),'vars',{u});
+            ht_3 = matlabFunction(tgt(3),'vars',{u}); 
+            
+%             x0 = (u_min + u_max)/2;
+            lb = [u_min(1, j - 1), u_min(1, j), u_min(1, j + 1), ...
+                u_min(2, j - 1), u_min(2, j), u_min(2, j + 1), ...
+                u_min(3, j - 1), u_min(3, j), u_min(3, j + 1)];
+            ub = [u_max(1, j - 1), u_max(1, j), u_max(1, j + 1),...
+                u_max(2, j - 1), u_max(2, j), u_max(2, j + 1),...
+                u_max(3, j - 1), u_max(3, j), u_max(3, j + 1)];
+            x0 = (lb + ub)/2;
+            
+            [list_1, fmin1] = fmincon(ht_1, x0,[],[],[],[], lb, ub);
+            [list_2, fmin2] = fmincon(ht_2, x0,[],[],[],[], lb, ub);
+            [list_3, fmin3] = fmincon(ht_3, x0,[],[],[],[], lb, ub);
+            
+            minlist(1,j) = fmin1;
+            minlist(2,j) = fmin2;
+            minlist(3,j) = fmin3;
+%             disp(fmin1);
+%             disp(fmin2);
+%             disp(fmin3);
         end 
         
-    end
+
     
     %solve 3 optimization problems at each stage
-    ht_1 = matlabFunction(tgt(1),'vars',{u});
-    ht_2 = matlabFunction(tgt(2),'vars',{u});
-    ht_3 = matlabFunction(tgt(3),'vars',{u});    
+%     ht_1 = matlabFunction(tgt(1),'vars',{u});
+%     ht_2 = matlabFunction(tgt(2),'vars',{u});
+%     ht_3 = matlabFunction(tgt(3),'vars',{u});    
+%     
+%     x0 = (u_min + u_max)/2;
+%     lb = u_min;
+%     ub = u_max;
+% 
+%     [list_1, ~] = fmincon(ht_1, x0,[],[],[],[], lb, ub);
+%     [list_2, ~] = fmincon(ht_2, x0,[],[],[],[], lb, ub);
+%     [list_3, ~] = fmincon(ht_3, x0,[],[],[],[], lb, ub);
     
-    x0 = (u_min + u_max)/2;
-    lb = u_min;
-    ub = u_max;
-
-    [list_1, ~] = fmincon(ht_1, x0,[],[],[],[], lb, ub);
-    [list_2, ~] = fmincon(ht_2, x0,[],[],[],[], lb, ub);
-    [list_3, ~] = fmincon(ht_3, x0,[],[],[],[], lb, ub);
     
-    
-    for j = 2 : m - 1  %calculate rho for next step
-        
-        minlist_1(:, 1) = [0.5; 0; 0.7];      %BC conditions
-        minlist_1(:, m) = [1.5; 0; 1.5];  
-        
-        k1 = list_1(:, j - 1);
-        k2 = list_1(:, j);        
-        k3 = list_1(:, j + 1);
-
-        
-
-        u_1 = (k3 + k2)/2 - dt/(2 * dx) * (fun(k3(1), k3(2), k3(3), gamma) - fun(k2(1), k2(2), k2(3), gamma));  %u_+1/2
-        u_2 = (k2 + k1)/2 - dt/(2 * dx) * (fun(k2(1), k2(2), k2(3), gamma) - fun(k1(1), k1(2), k1(3), gamma));  %u_-1/2
-
-        minlist_1(:, j) = k2 - dt/dx * (fun(u_1(1), u_1(2), u_1(3), gamma) - fun(u_2(1), u_2(2), u_2(3), gamma));     %f(u_i+1/2)-f(u_i-1/2)    
-        
-    end        
-    
-    for j = 2 : m - 1  %calculate momentum for next step
-        
-        minlist_2(:, 1) = [0.5; 0; 0.7];      %BC conditions
-        minlist_2(:, m) = [1.5; 0; 1.5];  
-        
-        k1 = list_2(:, j - 1);
-        k2 = list_2(:, j);        
-        k3 = list_2(:, j + 1);
-
-        
-
-        u_1 = (k3 + k2)/2 - dt/(2 * dx) * (fun(k3(1), k3(2), k3(3), gamma) - fun(k2(1), k2(2), k2(3), gamma));  %u_+1/2
-        u_2 = (k2 + k1)/2 - dt/(2 * dx) * (fun(k2(1), k2(2), k2(3), gamma) - fun(k1(1), k1(2), k1(3), gamma));  %u_-1/2
-
-        minlist_2(:, j) = k2 - dt/dx * (fun(u_1(1), u_1(2), u_1(3), gamma) - fun(u_2(1), u_2(2), u_2(3), gamma));     %f(u_i+1/2)-f(u_i-1/2)    
-        
-    end    
-    
-    for j = 2 : m - 1  %calculate energy for next step
-        
-        minlist_3(:, 1) = [0.5; 0; 0.7];      %BC conditions
-        minlist_3(:, m) = [1.5; 0; 1.5];  
-        
-        k1 = list_3(:, j - 1);
-        k2 = list_3(:, j);        
-        k3 = list_3(:, j + 1);
-
-        
-
-        u_1 = (k3 + k2)/2 - dt/(2 * dx) * (fun(k3(1), k3(2), k3(3), gamma) - fun(k2(1), k2(2), k2(3), gamma));  %u_+1/2
-        u_2 = (k2 + k1)/2 - dt/(2 * dx) * (fun(k2(1), k2(2), k2(3), gamma) - fun(k1(1), k1(2), k1(3), gamma));  %u_-1/2
-
-        minlist_3(:, j) = k2 - dt/dx * (fun(u_1(1), u_1(2), u_1(3), gamma) - fun(u_2(1), u_2(2), u_2(3), gamma));  %f(u_i+1/2)-f(u_i-1/2)    
-        
-    end   
-    
-    minlist = [minlist_1(1, :); minlist_2(2, :); minlist_3(3, :)];
+%     for j = 2 : m - 1  %calculate rho for next step
+%         
+%         minlist_1(:, 1) = [0.5; 0; 0.7];      %BC conditions
+%         minlist_1(:, m) = [1.5; 0; 1.5];  
+%         
+%         k1 = list_1(:, j - 1);
+%         k2 = list_1(:, j);        
+%         k3 = list_1(:, j + 1);
+% 
+%         
+% 
+%         u_1 = (k3 + k2)/2 - dt/(2 * dx) * (fun(k3(1), k3(2), k3(3), gamma) - fun(k2(1), k2(2), k2(3), gamma));  %u_+1/2
+%         u_2 = (k2 + k1)/2 - dt/(2 * dx) * (fun(k2(1), k2(2), k2(3), gamma) - fun(k1(1), k1(2), k1(3), gamma));  %u_-1/2
+% 
+%         minlist_1(:, j) = k2 - dt/dx * (fun(u_1(1), u_1(2), u_1(3), gamma) - fun(u_2(1), u_2(2), u_2(3), gamma));     %f(u_i+1/2)-f(u_i-1/2)    
+%         
+%     end        
+%     
+%     for j = 2 : m - 1  %calculate momentum for next step
+%         
+%         minlist_2(:, 1) = [0.5; 0; 0.7];      %BC conditions
+%         minlist_2(:, m) = [1.5; 0; 1.5];  
+%         
+%         k1 = list_2(:, j - 1);
+%         k2 = list_2(:, j);        
+%         k3 = list_2(:, j + 1);
+% 
+%         
+% 
+%         u_1 = (k3 + k2)/2 - dt/(2 * dx) * (fun(k3(1), k3(2), k3(3), gamma) - fun(k2(1), k2(2), k2(3), gamma));  %u_+1/2
+%         u_2 = (k2 + k1)/2 - dt/(2 * dx) * (fun(k2(1), k2(2), k2(3), gamma) - fun(k1(1), k1(2), k1(3), gamma));  %u_-1/2
+% 
+%         minlist_2(:, j) = k2 - dt/dx * (fun(u_1(1), u_1(2), u_1(3), gamma) - fun(u_2(1), u_2(2), u_2(3), gamma));     %f(u_i+1/2)-f(u_i-1/2)    
+%         
+%     end    
+%     
+%     for j = 2 : m - 1  %calculate energy for next step
+%         
+%         minlist_3(:, 1) = [0.5; 0; 0.7];      %BC conditions
+%         minlist_3(:, m) = [1.5; 0; 1.5];  
+%         
+%         k1 = list_3(:, j - 1);
+%         k2 = list_3(:, j);        
+%         k3 = list_3(:, j + 1);
+% 
+%         
+% 
+%         u_1 = (k3 + k2)/2 - dt/(2 * dx) * (fun(k3(1), k3(2), k3(3), gamma) - fun(k2(1), k2(2), k2(3), gamma));  %u_+1/2
+%         u_2 = (k2 + k1)/2 - dt/(2 * dx) * (fun(k2(1), k2(2), k2(3), gamma) - fun(k1(1), k1(2), k1(3), gamma));  %u_-1/2
+% 
+%         minlist_3(:, j) = k2 - dt/dx * (fun(u_1(1), u_1(2), u_1(3), gamma) - fun(u_2(1), u_2(2), u_2(3), gamma));  %f(u_i+1/2)-f(u_i-1/2)    
+%         
+%     end   
+%     
+%     minlist = [minlist_1(1, :); minlist_2(2, :); minlist_3(3, :)];
 end
--- engine/reach_sys_nonlnhypo.m
+++ engine/reach_sys_nonlnhypo.m
@@ -1,44 +1,50 @@
 function [sol_min, sol_max] = reach_sys_nonlnhypo(gamma, deltat, deltax, init_min, init_max, time, xlist, tlist)
-
+    tic;
     u_min = init_min;
     u_max = init_max;
     mesh = size(xlist);
     m = mesh(2);    %mesh points
 
-    sol_min = solve_sys_nonlnhypo(gamma, deltat, deltax, init_min, time, xlist, tlist);
-    sol_max = solve_sys_nonlnhypo(gamma, deltat, deltax, init_max, time, xlist, tlist);
+%     sol_min = solve_sys_nonlnhypo(gamma, deltat, deltax, init_min, time, xlist, tlist);
+%     sol_max = solve_sys_nonlnhypo(gamma, deltat, deltax, init_max, time, xlist, tlist);
 
-
+    sol_min = zeros(3, m, time);
+    sol_max = zeros(3, m, time);
+    
     for i = 1 : time - 1    
 
         u_min_new = min_sys_nonlnhypo(u_min, u_max, deltat, deltax, xlist, gamma);
         u_max_new = max_sys_nonlnhypo(u_min, u_max, deltat, deltax, xlist, gamma); 
-
-        for j = 1 : m       %for the purpose of soundness, adding max and min value at each step
-            for k = 1 : 3
-
-                if u_min_new(k, j) > sol_min(k, j, i + 1)
-                    u_min_new(k, j) = sol_min(k, j, i + 1);
-                end
-
-                if u_max_new(k, j) < sol_max(k, j, i + 1)
-                    u_max_new(k, j) = sol_max(k, j, i + 1);
-                end
-
-            end
-        end
-
+% 
+%         for j = 1 : m       %for the purpose of soundness, adding max and min value at each step
+%             for k = 1 : 3
+% 
+%                 if u_min_new(k, j) > sol_min(k, j, i + 1)
+%                     u_min_new(k, j) = sol_min(k, j, i + 1);
+%                 end
+% 
+%                 if u_max_new(k, j) < sol_max(k, j, i + 1)
+%                     u_max_new(k, j) = sol_max(k, j, i + 1);
+%                 end
+% 
+%             end
+%         end
+% 
         sol_min(:, :, i + 1) = u_min_new;
         sol_max(:, :, i + 1) = u_max_new;
 
         u_min = u_min_new;
         u_max = u_max_new;
 
+        disp(u_min);
+        disp(u_max);
     end
 
-
-    plot_2dboxbald(sol_min(1, :,41), sol_max(1, :,41), deltax, xlist);      % t = 4      
-    plot_fixlocbox(sol_min(3, 5, :), sol_max(3, 5, :), deltat, tlist);      % x = 4
+    toc;
+    
+    
+%     plot_2dboxbald(sol_min(1, :,41), sol_max(1, :,41), deltax, xlist, 0);      % t = 4      
+%     plot_fixlocbox(sol_min(3, 5, :), sol_max(3, 5, :), deltat, tlist);      % x = 4
 
 %---------------rho plotting--------------------------------
     sol_min_rho = squeeze(sol_min(1, :, :))';
